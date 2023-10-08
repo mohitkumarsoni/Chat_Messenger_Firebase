1161fb46f844b672577ca1e688204cadaa439ed7
mohit soni <mohitkumarsoni135@gmail.com>
2023-10-08 12:05:58 +0530

    initial commit

---
+++ app/src/main/java/com/example/chatmessenger/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatmessenger.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatmessenger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
